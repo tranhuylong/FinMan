4350da07ebf816a37073ddc227f2db43628105f8
tranhuylong <long.tranhuy@gmail.com>
2017-05-25 21:19:28 +0700

    Add files via upload

---
+++ FinMan/FinMan/Income.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using FinMan.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FinMan
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Income : Page
+    {
+        public Income()
+        {
+            this.InitializeComponent();
+            incomeLoad();
+            cb_Income.SelectedIndex = 0;
+        }
+
+        public void incomeLoad()
+        {
+            var str = File.ReadAllText(@"income.json");
+            List<string> jins = JsonConvert.DeserializeObject<List<string>>(str);
+            foreach(string x in jins)
+            {
+                cb_Income.Items.Add(x);
+            }
+        }
+
+        private void cb_Income_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //this.Frame.Navigate(typeof(IncomeValue), null);
+        }
+
+        private void btn_vIncome_Click(object sender, RoutedEventArgs e)
+        {
+            Action act = new Action
+            {
+                Name = cb_Income.SelectedValue.ToString(),
+                Value = txt_vIncome.Text.ToString()
+            };
+            //Task.Run(() => File.WriteAllText(@"h:/action.json", JsonConvert.SerializeObject(act)));
+            Task.Run(() => {
+                using (StreamWriter file = File.CreateText(@"h:/action.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, act);
+                }
+            });
+            /**/
+        }
+    }
+}
---
+++ FinMan/FinMan/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FinMan
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btn_Income_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Income), null);
+        }
+
+        private void btn_Outcome_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Outcome), null);
+        }
+
+        private void btn_Statistical_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Summary), null);
+        }
+    }
+}
