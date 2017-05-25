28c51f522f6999e96fb8ed66a1ae9478da14eb45
tranhuylong <long.tranhuy@gmail.com>
2017-05-25 21:22:38 +0700

    Add files via upload

---
+++ FinMan/FinMan/Model/finance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinMan.Model
+{
+    public class finance
+    {
+        private int _ActType;
+        public int ActType { get => _ActType; set => _ActType = value; }
+
+        
+    }
+
+}
---
+++ FinMan/FinMan/Model/jsonDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinMan.Model
+{
+    public class Jincome
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Jincomes
+    {
+        public List<Jincome> jins { get; set; }
+    }
+
+    public class Action
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+
+}
